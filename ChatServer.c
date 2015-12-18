b1aa3e52da6adae9d5dbd45660cd54d9ebcab2d1
JJ <joshuajordanlowe@gmail.com>
2015-12-17 17:22:09 -0700

    Small formatting changes + ability to input port

--- ChatServer.c
+++ ChatServer.c
@@ -5,83 +5,109 @@
 #include"string.h"
 #include"netinet/in.h"
 #include"pthread.h"
- 
-#define PORT 4444
+
 #define BUF_SIZE 2000
 #define CLADDR_LEN 100
  
-void * receiveMessage(void * socket) {
+void * receiveMessage(void * socket) 
+{
     int sockfd, ret;
     char buffer[BUF_SIZE];
     sockfd = (int) socket;
     memset(buffer, 0, BUF_SIZE);
-    for (;;) {
+
+    for (;;) 
+    {
         ret = recvfrom(sockfd, buffer, BUF_SIZE, 0, NULL, NULL);
-        if (ret < 0) {
-            printf("Error receiving data!\n");
-        } else {
-            printf("client: ");
+
+        if (ret < 0) 
+            printf("[-] Error receiving data\n");
+        else 
+        {
+            printf("C: ");
             fputs(buffer, stdout);
             //printf("\n");
         }
     }
 }
  
-void main() {
+void main(int argc, char**argv) 
+{
     struct sockaddr_in addr, cl_addr;
-    int sockfd, len, ret, newsockfd;
+    int sockfd, len, ret, newsockfd, port;
     char buffer[BUF_SIZE];
     pid_t childpid;
     char clientAddr[CLADDR_LEN];
     pthread_t rThread;
- 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) {
-        printf("Error creating socket!\n");
+
+    if (argc < 2) 
+    {
+        printf("usage: server \n");
         exit(1);
     }
-    printf("Socket created...\n");
+ 
+    port = atoi(argv[1]);
+ 
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (sockfd < 0) 
+    {
+        printf("[-] Error creating socket\n");
+        exit(1);
+    }
+
+    printf("[+] Socket created\n");
  
     memset(&addr, 0, sizeof(addr));
     addr.sin_family = AF_INET;
     addr.sin_addr.s_addr = INADDR_ANY;
-    addr.sin_port = PORT;
+    addr.sin_port = port;
  
     ret = bind(sockfd, (struct sockaddr *) &addr, sizeof(addr));
     if (ret < 0) {
-        printf("Error binding!\n");
+        printf("[-] Error binding\n");
         exit(1);
     }
-    printf("Binding done...\n");
+    printf("[+] Binding done\n");
  
-    printf("Waiting for a connection...\n");
+    printf("[+] Waiting for a connection...\n");
     listen(sockfd, 5);
  
-    for (;;) { //infinite loop
+    for (;;) 
+    { 
         len = sizeof(cl_addr);
         newsockfd = accept(sockfd, (struct sockaddr *) &cl_addr, &len);
-        if (newsockfd < 0) {
-            printf("Error accepting connection!\n");
+
+        if (newsockfd < 0) 
+        {
+            printf("[-] Error accepting connection\n");
             exit(1);
         }
-        printf("Connection accepted...\n");
+
+        printf("[+] Connection accepted\n");
  
         inet_ntop(AF_INET, &(cl_addr.sin_addr), clientAddr, CLADDR_LEN);
-        if ((childpid = fork()) == 0) { //creating a child process
+
+        if ((childpid = fork()) == 0) 
+        { //creating a child process
  
             close(sockfd); //stop listening for new connections
  
             //creating a new thread for receiving messages from the client
             ret = pthread_create(&rThread, NULL, receiveMessage, (void *) newsockfd);
-            if (ret) {
-                printf("ERROR: Return Code from pthread_create() is %d\n", ret);
+            if (ret) 
+            {
+                printf("[-] Return Code from pthread_create() is %d\n", ret);
                 exit(1);
             }
  
-            while (fgets(buffer, BUF_SIZE, stdin) != NULL) {
+            while (fgets(buffer, BUF_SIZE, stdin) != NULL) 
+            {
                 ret = sendto(newsockfd, buffer, BUF_SIZE, 0, (struct sockaddr *) &cl_addr, len);
-                if (ret < 0) {
-                    printf("Error sending data!\n");
+
+                if (ret < 0) 
+                {
+                    printf("[-] Error sending data\n");
                     exit(1);
                 }
             }
